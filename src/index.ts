5c83bd9afbc03344525c3f17345ba85da85decc9
ryanbetts <84613835+depatchedmode@users.noreply.github.com>
2024-02-07 11:17:43 -0800

    fix: bad input path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import generateWord from "./verbs/generate.js";
 import * as phonemes from "./elements/phonemes.js";
 import * as graphemes from "./elements/graphemes.js";
-import { setRand } from "./utils/random";
+import setRand from "./utils/setRand.js";
 
 export default {
   generateWord,
--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./getWeightedOption.js";
-export * from "./pick.js";
-export * from "./random.js";
-export * from "./randomBool.js";
-export * from "./randomRange.js";
-export * from "./setRand.js";
